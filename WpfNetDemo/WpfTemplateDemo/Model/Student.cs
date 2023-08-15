5d0732f5ace81891a75d2014e34e36038c5323ce
Landon <Landon@email.com>
2023-08-15 23:39:53 +0800

    Style的Setter和Trigger

---
+++ WpfNetDemo/WpfTemplateDemo/Model/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfTemplateDemo.Model
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Skill { get; set; }
+        public bool HasJob { get; set; }
+    }
+}
