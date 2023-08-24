1543aa55e450c8153cf229455ee4eed1d5a55edf
CaoShangXi <1427026071qq@sina.com>
2023-08-25 00:58:45 +0800

    代码文件移到View文件夹下

---
+++ WpfNetDemo/WpfTemplateDemo/View/MultiDataTrigger.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WpfTemplateDemo.Model;
+
+namespace WpfTemplateDemo
+{
+    /// <summary>
+    /// MultiDataTrigger.xaml 的交互逻辑
+    /// </summary>
+    public partial class MultiDataTrigger : Window
+    {
+        public MultiDataTrigger()
+        {
+            InitializeComponent();
+            DataContext = new List<Student> {
+            new Student{
+            Id=1,
+            Name="Jack",
+            },
+            new Student{
+            Id=2,
+            Name="Tom",
+            },
+            new Student{
+            Id=3,
+            Name="Bob",
+            },
+            };
+        }
+    }
+}
