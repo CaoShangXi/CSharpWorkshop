27c44485320fa6d5130a022a0d8f2fef10bcbf7b
CaoShangXi <1427026071qq@sina.com>
2023-08-25 22:48:14 +0800

    10.2且”静“且”动“用资源

--- WpfNetDemo/WpfTemplateDemo/View/ResourcesDemo.xaml.cs
+++ WpfNetDemo/WpfTemplateDemo/View/ResourcesDemo.xaml.cs
@@ -23,5 +23,19 @@ namespace WpfTemplateDemo.View
         {
             InitializeComponent();
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            string text=(string)this.FindResource("str");
+            string text1=(string)Resources["str"];
+            Console.WriteLine(text1);
+            Console.WriteLine(text);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Resources["res1"] = new TextBlock { Text="天涯共此时"};
+            Resources["res2"] = new TextBlock { Text = "天涯共此时"};
+        }
     }
 }
