10a2026b78804a44d0d09be37da4f9d25cd95b56
Landon <1427026071@qq.com>
2024-01-26 22:23:25 +0800

    6.3.4“没有Path”的Binding

---
+++ WpfNetDemo/WpfTemplateDemo/View/BindingPathDemo.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfTemplateDemo.View
+{
+    /// <summary>
+    /// BindingPathDemo.xaml 的交互逻辑
+    /// </summary>
+    public partial class BindingPathDemo : Window
+    {
+        public BindingPathDemo()
+        {
+            InitializeComponent();
+            string myString = "菩提本无树，明镜亦非台。本来无一物，何处惹尘埃。";
+            this.textBlock1.SetBinding(TextBlock.TextProperty,new Binding(".") { Source=myString});
+        }
+    }
+}
