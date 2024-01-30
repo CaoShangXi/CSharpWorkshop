30dabe57e47fff6840dc6292d8a9e1f7846f45b1
Landon <1427026071@qq.com>
2024-01-30 22:20:35 +0800

    6.3.11 使用 ObjectDataProvider对象作为 Binding的Source

---
+++ WpfNetDemo/WpfTemplateDemo/Model/Calculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfTemplateDemo.Model
+{
+    /// <summary>
+    /// 计算器
+    /// </summary>
+    public class Calculator
+    {
+        //加法
+        public string Add(string argl, string arg2)
+        {
+            double x = 0;
+            double y = 0;
+            double z = 0;
+            if (double.TryParse(argl, out x) && double.TryParse(arg2, out y))
+            {
+                z = x + y;
+                return z.ToString();
+            }
+            return "Input Error!";
+        }
+    }
+}
