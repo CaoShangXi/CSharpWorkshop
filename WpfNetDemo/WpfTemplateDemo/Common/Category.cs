79ece48c8f8cb1f0b36ff2c280fe7b10c2689826
Landon <1427026071@qq.com>
2024-02-01 01:00:26 +0800

    6.4.2 Binding的数据转换

---
+++ WpfNetDemo/WpfTemplateDemo/Common/Category.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfTemplateDemo.Common
+{
+    /// <summary>
+    /// 种类
+    /// </summary>
+    public enum Category
+    {
+        /// <summary>
+        /// 轰炸机
+        /// </summary>
+        Bomber,
+        /// <summary>
+        /// 战斗机
+        /// </summary>
+        Fighter
+    }
+}
